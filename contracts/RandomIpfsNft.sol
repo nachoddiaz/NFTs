69d9c9adcaa3cafa25a27a5b66306bab74a7f2b2
nachoddiaz <nachoddiaz@LAPTOP-UUCHMGTT.localdomain>
2022-08-22 22:42:55 +0200

    update

---
+++ contracts/RandomIpfsNft.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract RandomIpfsNFT {
+
+
+    constructor() {
+        
+    }
+
+    function requestNft(){
+
+    }
+
+    function fulfillRandomWords(uint256 requestId, unit256[] memory RandomWords){
+        
+    }
+}
