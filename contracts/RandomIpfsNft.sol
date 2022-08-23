8f2c778bc58886384258314b15ecd624ee22efbd
nachoddiaz <nachoddiaz@LAPTOP-UUCHMGTT.localdomain>
2022-08-23 18:59:32 +0200

    update

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -2,19 +2,31 @@
 pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract RandomIpfsNFT {
+contract RandomIpfsNFT is VRFConsumerBaseV2{
 
 
-    constructor() {
+    constructor(address vrfCoordinatorV2) VRFConsumerBaseV2(vrfCoordinatorV2){
         
     }
 
-    function requestNft(){
+    function requestNft() public {
+    s_requestId = COORDINATOR.requestRandomWords(
+      keyHash,
+      s_subscriptionId,
+      requestConfirmations,
+      callbackGasLimit,
+      numWords
+    );
+    }
+
+    function fulfillRandomWords(uint256 requestId, unit256[] memory RandomWords) internal override{
 
     }
 
-    function fulfillRandomWords(uint256 requestId, unit256[] memory RandomWords){
-        
+    function tokenURI(uint256 tokenId) public view override returns(string memory){
+        return TOKEN_URI;
     }
 }
