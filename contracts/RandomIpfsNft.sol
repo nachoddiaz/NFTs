e419d77a0cda2cd8ba5bf3a8dfd30de3cfc48475
nachoddiaz <nachoddiaz@LAPTOP-UUCHMGTT.localdomain>
2022-08-23 21:39:42 +0200

    3update

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -1,12 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
+//Custom Errors
+error RandomIpfsNFT__RangeOutOfBounds();
 
-contract RandomIpfsNFT is VRFConsumerBaseV2, ERC721 {
+contract RandomIpfsNFT is VRFConsumerBaseV2, ERC721URIStorage {
+
+    //Type Declaration
+    enum NFTType {
+        PUG,
+        SHIBA_INU,
+        ST_BERNARD
+    }
 
     //ChainLink VRF Variables
     VRFCoordinatorV2Interface private immutable i_vrfCordinator;
@@ -22,17 +31,20 @@ contract RandomIpfsNFT is VRFConsumerBaseV2, ERC721 {
     //NFT variables
     uint256 private s_tokenCounter;
     uint256 internal constant MAX_CHANCE_VALUE=100;
+    string[] internal s_NFTTokenURI;
 
     constructor(
         address vrfCoordinatorV2,
         uint64 subscriptionId,
         bytes32 gasLane,
-        uint32 callbackGasLimit
+        uint32 callbackGasLimit,
+        string[3] memory NFTTokenURI
     ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("Random IPFS NFT", "RIN"){
         i_vrfCordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_subscriptionId = subscriptionId;
         i_gasLane = gasLane;
         i_callbackGasLimit = callbackGasLimit;
+        s_NFTTokenURI = NFTTokenURI;
     }
 
     function requestNft() public returns (uint256 requestId) {
@@ -52,12 +64,26 @@ contract RandomIpfsNFT is VRFConsumerBaseV2, ERC721 {
     {
         address NFTOwner = s_requestIdToSender[requestId];
         uint256 newTokenId = s_tokenCounter;
-        _safeMint(NFTOwner, newTokenId);
-        s_tokenCounter++;
 
         //coge el resto entre la primera palabra random y el máximo número de probabilidad
         uint256 moddedRnd = RandomWords[0] % MAX_CHANCE_VALUE;
+        NFTType tipo = getRarityFromModdedRnd(moddedRnd);
+        _safeMint(NFTOwner, newTokenId);
+        _setTokenURI(newTokenId, s_NFTTokenURI[uint256(tipo)]);
+        s_tokenCounter++;
 
+    }
+
+    function getRarityFromModdedRnd(uint256 moddedRnd) public pure returns(NFTType){
+        uint256 cumulativeSum =0;
+        uint256[3] memory chanceArray = getChanceArray();
+        for(uint256 i=0; i<chanceArray.length; i++){
+            if (moddedRnd >= cumulativeSum && moddedRnd < cumulativeSum + chanceArray[i]){
+                return NFTType(i);
+            }
+            cumulativeSum += chanceArray[i];
+        }
+        revert RandomIpfsNFT__RangeOutOfBounds();
     }
 
     function getChanceArray() public pure returns (uint256[3] memory) {
